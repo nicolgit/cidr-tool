4b779cc84bf1019a753c400057f9379c864eeb58
Nicola Delfino <nicolgit@users.noreply.github.com>
2026-09-23 10:28:19 +0200

    google analytics

---
+++ src/analytics.js
@@ -0,0 +1,9 @@
+var googleTagScript = document.createElement('script');
+googleTagScript.async = true;
+googleTagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-K7V8GGF5Z5';
+document.head.appendChild(googleTagScript);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-K7V8GGF5Z5');
